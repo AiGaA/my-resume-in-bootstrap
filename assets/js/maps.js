00d6a9ac9d3da24404d28c56bf511b0376e883b3
Aiga <aiga.andrejeva@gmail.com>
2023-08-07 14:03:11 +0000

    Hide the API key

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
+
+apiKey = process.env.API_KEY;
+
 function initMap() {
+
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
